3f34615c17d01c2611005a95437f8bfdaab32a95
Amber Schaack <aschaack9@gmail.com>
2024-04-18 15:34:47 -0500

    updated search function to find tracks

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,6 +3,55 @@ console.log('***** Music Collection *****');
 
 let myCollection = [];
 
+const badlandsTracks = [
+  { number: 1, name: 'Drive', duration: '4:19' },
+  { number: 2, name: 'Castle', duration: '4:38' },
+  { number: 3, name: 'Ghost', duration: '2:34' },
+];
+
+const anonymousTracks = [
+  { number: 1, name: 'PINK ROLEX', duration: '4:19' },
+  { number: 2, name: 'MAKE A MESS', duration: '4:38' },
+  { number: 3, name: 'CHANGES', duration: '2:34' },
+];
+
+const rollTracks = [
+  { number: 1, name: 'Unlucky Sin', duration: '4:19' },
+  { number: 2, name: 'Georgia Moon', duration: '4:38' },
+  { number: 3, name: 'Built to Roam', duration: '2:34' },
+];
+
+const funhouseTracks = [
+  { number: 1, name: 'So What', duration: '4:19' },
+  { number: 2, name: 'Sober', duration: '4:38' },
+  { number: 3, name: 'Crystal Ball', duration: '2:34' },
+];
+
+const purgatoryTracks = [
+  { number: 1, name: 'I Swear', duration: '4:19' },
+  { number: 2, name: 'Tattoos', duration: '4:38' },
+  { number: 3, name: 'Whitehouse Road', duration: '2:34' },
+];
+
+const happierTracks = [
+  { number: 1, name: 'Getting Older', duration: '4:19' },
+  { number: 2, name: 'Oxytocin', duration: '4:38' },
+  { number: 3, name: 'Lost Cause', duration: '2:34' },
+];
+
+const countryTracks = [
+  { number: 1, name: 'House Fire', duration: '4:19' },
+  { number: 2, name: 'Creeker', duration: '4:38' },
+  { number: 3, name: 'Peace of Mind', duration: '2:34' },
+];
+
+const digitalTracks = [
+  { number: 1, name: 'wish u the best', duration: '4:19' },
+  { number: 2, name: 'double', duration: '4:38' },
+  { number: 3, name: 'moodz', duration: '2:34' },
+];
+
+/*
 function addToCollection(collection, title, artist, yearPublished) {
   const album = {
     collection: collection,
@@ -13,30 +62,99 @@ function addToCollection(collection, title, artist, yearPublished) {
   collection.push(album);
   return album;
 }
-console.log(addToCollection(myCollection, 'Badlands', 'Halsey', 2015));
-console.log(addToCollection(myCollection, 'ANONYMOUS', 'blackbear', 2019));
+*/
+
+// extra stretchy addToCollection
+
+function addToCollection(collection, title, artist, yearPublished, tracks) {
+  const album = {
+    collection: collection,
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished,
+    tracks: tracks,
+  };
+  collection.push(album);
+  return album;
+}
+
 console.log(
-  addToCollection(myCollection, 'Roll the Bones', 'Shakey Graves', 2011)
+  addToCollection(myCollection, 'Badlands', 'Halsey', 2015, badlandsTracks)
 );
-console.log(addToCollection(myCollection, 'Funhouse', 'Pink', 2008));
-console.log(addToCollection(myCollection, 'Purgatory', 'Tyler Childers', 2017));
 console.log(
-  addToCollection(myCollection, 'Happier Than Ever', 'Billie Eilish', 2021)
+  addToCollection(myCollection, 'ANONYMOUS', 'blackbear', 2019, anonymousTracks)
 );
 console.log(
-  addToCollection(myCollection, 'Country Squire', 'Tyler Childers', 2019)
+  addToCollection(
+    myCollection,
+    'Roll the Bones',
+    'Shakey Graves',
+    2011,
+    rollTracks
+  )
 );
 console.log(
-  addToCollection(myCollection, 'digital druglord', 'blackbear', 2017)
+  addToCollection(myCollection, 'Funhouse', 'Pink', 2008, funhouseTracks)
+);
+console.log(
+  addToCollection(
+    myCollection,
+    'Purgatory',
+    'Tyler Childers',
+    2017,
+    purgatoryTracks
+  )
+);
+console.log(
+  addToCollection(
+    myCollection,
+    'Happier Than Ever',
+    'Billie Eilish',
+    2021,
+    happierTracks
+  )
+);
+console.log(
+  addToCollection(
+    myCollection,
+    'Country Squire',
+    'Tyler Childers',
+    2019,
+    countryTracks
+  )
+);
+console.log(
+  addToCollection(
+    myCollection,
+    'digital druglord',
+    'blackbear',
+    2017,
+    digitalTracks
+  )
 );
 
 console.log(myCollection);
+
+/*
+function showCollection(collection) {
+  for (i = 0; i < collection.length; i++) {
+    console.log(
+      `${collection[i].title} by ${collection[i].artist}, published in ${collection[i].yearPublished}`
+    );
+  }
+}
+*/
+
+// extra stretchy showCollection
 
 function showCollection(collection) {
   for (i = 0; i < collection.length; i++) {
     console.log(
       `${collection[i].title} by ${collection[i].artist}, published in ${collection[i].yearPublished}`
     );
+    for (let track of collection[i].tracks) {
+      console.log(`\t\t${track.number}. ${track.name}: ${track.duration}`);
+    }
   }
 }
 showCollection(myCollection);
@@ -58,6 +176,7 @@ console.log(findByArtist(myCollection, 'blackbear'));
 
 // begin stretch goals
 
+/*
 let searchCriteria = { artist: 'blackbear', year: 2019 };
 function search(collection, searchCriteria) {
   let newCollection = [];
@@ -79,29 +198,26 @@ function search(collection, searchCriteria) {
   }
   return newCollection;
 }
+*/
+
+// extra stretchy searchCriteria
+
+let searchCriteria = { artist: 'blackbear', year: 2017, track: 'moodz' };
+function search(collection, searchCriteria) {
+  for (i = 0; i < collection.length; i++) {
+    console.log(
+      `${collection[i].title} by ${collection[i].artist}, published in ${collection[i].yearPublished}`
+    );
+    for (let track of collection[i].tracks) {
+      // console.log(track);
+      if (track.name === searchCriteria.track) {
+        return track.name;
+      }
+    }
+  }
+}
 
 console.log(search(myCollection, searchCriteria));
-
-/*
-let tracks0 = [{name: ['Drive', 'Castle', 'Ghost']}, {duration: ['4:19', '4:38', '2:34']}];
-let tracks1 = [{name: ['PINK ROLEX', 'MAKE A MESS', 'CHANGES'] }, {duration: ['2:17', '2:40', '2:34']}];
-let tracks2 = [{name: ['Unlucky Sin', 'Georgia Moon', 'Built to Roam']}, {duration: ['3:18', '2:43', '3:11']}];
-let tracks3 = [{name: ['So What', 'Sober', 'Crystal Ball']}, {duration: ['3:35', '4:12', '3:26']}];
-let tracks4 = [{name: ['I Swear', 'Tattoos', 'Whitehouse Road']}, {duration: ['3:21', '3:23', '4:41']}];
-let tracks5 = [{name: ['Getting Older', 'Oxytocin', 'Lost Cause']}, {duration: ['4:04', '3:30', '3:32']}];
-let tracks6 = [{name: ['Creeker', 'House Fire', 'Peace of Mind']}, {duration: ['5:01', '3:52', '4:42']}];
-let tracks7 = [{name: ['wish u the best', 'double', 'moodz']}, {duration: ['2:34', '3:00', '3:41']}];
-
-Obj['tracks'].myCollection[0].push(tracks0);
-console.log(myCollection[0]);
-myCollection[1].push(tracks1);
-myCollection[2].push(tracks2);
-myCollection[3].push(tracks3);
-myCollection[4].push(tracks4);
-myCollection[5].push(tracks5);
-myCollection[6].push(tracks6);
-myCollection[7].push(tracks7);
-*/
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
 // as a lil' chunk of friendly code that you don't need to understand right now.
